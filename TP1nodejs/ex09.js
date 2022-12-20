046a76b808a7ff634306562bae5f44727f759842
Adam Hocini <adamhocini@MacBook-Pro-de-Adam.local>
2022-12-20 10:56:21 +0100

    module nodeJS day 01

---
+++ TP1nodejs/ex09.js
@@ -0,0 +1,21 @@
+function countVowels(str) {
+    // On définit un compteur à 0
+    let count = 0;
+  
+    // On définit un tableau contenant les voyelles
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+    // On parcourt chaque caractère de la chaîne
+    for (let char of str) {
+      // Si le caractère est une voyelle, on incrémente le compteur
+      if (vowels.includes(char.toLowerCase())) {
+        count++;
+      }
+    }
+  
+    // On renvoie le nombre de voyelles
+    return count;
+  }
+
+  const numVowels = countVowels('Bonjour');
+console.log(numVowels); // Affiche "2"
