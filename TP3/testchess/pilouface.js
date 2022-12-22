3d4ffc2f9bc154ad6de95d9920b5b6451442315f
Adam Hocini <adamhocini@MacBook-Pro-de-Adam.local>
2022-12-22 13:35:04 +0100

    module nodeJS day 03

--- TP3/testchess/pilouface.js
+++ TP3/testchess/pilouface.js
@@ -1,16 +1,38 @@
 const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
+//readline fournit une interface pour lire les données d'un flux lisible
 
 let pilouface = (userChoice) => {
   
   let outcome = "f";
+  // La fonction Math.random() renvoie un nombre pseudo-aléatoire à virgule flottante supérieur ou égal à 0 et inférieur à 1
   if (Math.random() > 0.5) {
     outcome = "p";
         }
     if (outcome == userChoice) {
-      console.log("Vous avez gagné! :)");
+      console.log("Vous avez gagné! : ``)");
+      console.log(`
+      ░░░░░░░░░░░░░░░░░░░░░░█████████
+      ░░███████░░░░░░░░░░███▒▒▒▒▒▒▒▒███
+      ░░█▒▒▒▒▒▒█░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒▒▒███
+      ░░░█▒▒▒▒▒▒█░░░░██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+      ░░░░█▒▒▒▒▒█░░░██▒▒▒▒▒██▒▒▒▒▒▒██▒▒▒▒▒███
+      ░░░░░█▒▒▒█░░░█▒▒▒▒▒▒████▒▒▒▒████▒▒▒▒▒▒██
+      ░░░█████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+      ░░░█▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒██
+      ░██▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒▒██
+      ██▒▒▒███████████▒▒▒▒▒██▒▒▒▒▒▒▒▒██▒▒▒▒▒██
+      █▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒████████▒▒▒▒▒▒▒██
+      ██▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+      ░█▒▒▒███████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██
+      ░██▒▒▒▒▒▒▒▒▒▒████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+      ░░████████████░░░█████████████████
+      `);
     } else {
       console.log("vous avez perdu... :(");
+      console.log(`
+      ✭┈┈┈┈N┈O┈O┈B┈S┈┈┈✭
+      `)
     }
 
 }
@@ -18,4 +40,8 @@ let message = "Tapez `p` pour pile ou `f` pour face, puis appuyez sur Entrée.\n
 rl.question(message, (answer) => {
         pilouface(answer);
         rl.close();
+        //La méthode rl.close() ferme l'instance d'InterfaceConstructor et abandonne le contrôle des flux d'entrée et de sortie.
 });
+
+
+
