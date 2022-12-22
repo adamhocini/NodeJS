5ddbb15284613ab8feaee65e818d9050bb00efbc
Adam Hocini <adamhocini@MacBook-Pro-de-Adam.local>
2022-12-22 17:22:15 +0100

    module nodeJS day 03

--- TP3/testchess/pilouface.js
+++ TP3/testchess/pilouface.js
@@ -39,12 +39,15 @@ let pilouface = (userChoice) => {
 console.log(`
 
 
+
+
 $$___$$_ $$$$$$$_ $$______ $$______ __$$$___ 
 $$___$$_ $$______ $$______ $$______ _$$_$$__ 
 $$$$$$$_ $$$$$___ $$______ $$______ $$___$$_ 
 $$___$$_ $$______ $$______ $$______ $$___$$_ 
 $$___$$_ $$______ $$____$_ $$____$_ _$$_$$__ 
 $$___$$_ $$$$$$$_ $$$$$$$_ $$$$$$$_ __$$$___ 
+
 
 
 
@@ -56,8 +59,35 @@ rl.question(message, (answer) => {
         //La méthode rl.close() ferme l'instance d'InterfaceConstructor et abandonne le contrôle des flux d'entrée et de sortie.
 });
 
-         
+var prompt = require('prompt');
 
+prompt.get(['choice'], function(err, result) {
+    if (result.choice == "0") {
+          console.log('voulez-vous recommencer?')
+    }
+});
+
+
+let pilouface = (userChoice) => {
+  
+  let outcome = "f";
+  // La fonction Math.random() renvoie un nombre pseudo-aléatoire à virgule flottante supérieur ou égal à 0 et inférieur à 1
+  if (Math.random() > 0.5) {
+    outcome = "p";
+        }
+    if (outcome == userChoice) {
+      console.log("Vous avez gagné! : ``)"); 
+    } else {
+      console.log("vous avez perdu... :(");
+      console.log(`
+      ✭┈┈┈┈N┈O┈O┈B┈S┈┈┈✭
+      `)
+    }
+
+}
+         
+// ajouter un choix 
+// ajouter un compter de point 
 
 
 
